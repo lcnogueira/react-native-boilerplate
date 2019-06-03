7c79e77d54eb5f3f8a69e10528feac1fc562ba31
Luiz Claudio <luizcns18@gmail.com>
2019-06-03 13:16:39 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import '~/config/ReactotronConfig';
+import '~/config/StatusBarConfig';
+import React from 'react';
+import { Provider } from 'react-redux';
+
+import store from '~/store';
+
+import Routes from '~/routes';
+import { setNavigator } from './services/navigation';
+
+const App = () => (
+  <Provider store={store}>
+    <Routes ref={setNavigator} />
+  </Provider>
+);
+
+export default App;
